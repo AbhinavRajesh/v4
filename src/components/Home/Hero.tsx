1394bf3b29d0c51e301e184acd5b2c3c2c81601c
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2025-09-29 14:23:56 +0530

    chore: updated hero heading

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
           </div>
           <div className="flex flex-col justify-end flex-1">
             <div className="flex grow items-end pb-1 pl-4 bg-[repeating-linear-gradient(45deg,var(--pattern-foreground)_0,var(--pattern-foreground)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] [--pattern-foreground:var(--color-edge)]/56" />
-            <h1 className="text-3xl font-bold md:text-heading font-mono px-4 border-t border-edge">
+            <h1 className="text-2xl sm:text-3xl font-bold md:text-heading font-mono px-4 border-t border-edge">
               {config.userData.name}
             </h1>
             <p className="text-sm md:text-body border-t border-edge px-4 py-1">
